5f7f88f60df1ad0858041786ce482fadfa88d7e3
Marc Combarel <marc.combarel@gmail.com>
2020-09-14 19:31:25 +0200

    added transfer function

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,13 +1,47 @@
 pragma solidity ^0.5.0;
+// https://www.youtube.com/watch?time_continue=4&v=XdKv5uwEk5A&feature=emb_logo&ab_channel=DappUniversity
 
 contract DappToken {
-	// we need a constructor
-	// we need to be able to set the number of tokens 
-	// we need to read the number of tokens
-
+	// We set the global variables needed for our smart contract:
+	// • name of the token and symbol of the crypto
+	// • the supply amount of tokens
+	// • address and amount of tokens
+	string  public name   = 'Dapp Token';     // token name on the app
+	string  public symbol = 'DAPP';           // token name = currency name
+	// string  public standard = 'DAPP v1';   // token name = currency name
 	uint256 public totalSupply;
 
+	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+	mapping(address => uint256) public balanceOf;
+	
+	// we need to be able to set the number of tokens per account
+	// we need to read the number of tokens
+
+
+
+
+
+	// Initializer function:
 	constructor() public {
+		// • we set the initial supply of availoable token for our app
+		// • we allocate all the tokens to the admin (the one that called the function)
 		totalSupply = 100000;
+		balanceOf[msg.sender] = 100000;
+	}
+
+
+
+	// Transfer function
+	function transfer(address _to, uint256 _value) public returns(bool success){
+		// exception if not enough tokens
+		require(balanceOf[msg.sender] >= _value); // require --> if true, continue, if not stop	
+		// Now we need to transfer the balance
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+
+		Transfer(msg.sender, _to, _value);
+		return true;                              // need to return a boolean to be ERC20
+
 	}
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -3,17 +3,58 @@ var DappToken = artifacts.require("./DappToken.sol");
 
 // Test function:
 
-contract("DappToken", function(accounts){
-	it("set's the total supply upon deployement", function(){
-		
+contract("DappToken", function(accounts){ // we select the contratc and all the accounts registered (from ganache)
+	var tokenInstance;
+
+	it("initializes the contract with the correct values", function(){
+		return DappToken.deployed().then(function(instance){
+			tokenInstance = instance;
+			return tokenInstance.name();
+		}).then(function(name){
+			assert.equal(name, 'Dapp Token', 'has the correct name');
+			return tokenInstance.symbol();
+		}).then(function(symbol){
+			assert.equal(symbol, 'DAPP' , "has the correct symbol");
+		});
+	})
+
+
+	it("set's the total supply upon deployement", function(){	
 		// we read just like we did in the console
 		return DappToken.deployed().then(function(instance){
 			tokenInstance = instance;
 			return tokenInstance.totalSupply();
 		}).then(function(totalSupply){
-
 			// we test that the total supply is what we specified
 			assert.equal(totalSupply.toNumber(), 100000, "set's the total supply to 100,000");
+			return tokenInstance.balanceOf(accounts[0]);
+		}).then(function(adminBalance){
+			assert.equal(adminBalance.toNumber(), 100000, "allocating the initial supply to admin");
 		});
 	});
+
+
+
+	it("transfer token ownership", function(){
+		return DappToken.deployed().then(function(instance){
+			tokenInstance = instance;
+			// test `require`statement by transferring a larger amount than the account balance
+			return tokenInstance.transfer.call(accounts[1], 999999999999999);
+		}).then(assert.fail).catch(function(error){
+			assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
+			return tokenInstance.transfer(accounts[1],25000, { from: accounts[0]});
+		}).then(function(receipt){
+			return tokenInstance.balanceOf(accounts[1]);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), 25000, "adds the amount to the receiving accounts")
+			return tokenInstance.balanceOf(accounts[0]);
+		}).then(function(balance){
+			assert.equal(balance.toNumber(), 75000, "deducts the amount")
+		});
+	})
+
+
+
+
+
 });
